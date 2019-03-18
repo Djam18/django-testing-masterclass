30d7ceddea5e730585aa2e64002758fc1bf791f6
Abdel <abdel@dev.local>
2019-03-18 10:00:00 +0100

    init django testing masterclass

---
+++ README.md
@@ -0,0 +1,5 @@
+# Django Testing Masterclass
+
+Learning how to write proper tests for Django applications.
+
+Using pytest-django, factory_boy and Faker.
---
+++ testproject/testproject/settings.py
@@ -0,0 +1,37 @@
+SECRET_KEY = 'django-insecure-test-masterclass-key-abc123'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'blog',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'testproject.urls'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+STATIC_URL = '/static/'
